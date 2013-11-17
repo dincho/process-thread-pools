799f95a7ff07347ff5a3e0bcb167d536b0ac8b3a
Dincho Todorov <dincho@Dinchos-iMac.local>
2013-11-17 19:04:22 +0200

    Initial Commit

---
+++ ProcessPool/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  ProcessPool
+//
+//  Created by Dincho Todorov on 11/17/13.
+//  Copyright (c) 2013 Dincho Todorov. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
